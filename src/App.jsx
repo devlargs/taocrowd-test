e46bcfc16119f63de5c5e9f7a3a88ca76f9aa4c7
Ralph Largo <devlargs@gmail.com>
2025-02-18 18:41:22 +0800

    feat: added generic cards and loaders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  return <div className="App">Start coding!!!</div>;
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search" />
+      <section className="articles">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
+
+      <span class="loader"></span>
+    </div>
+  );
 }
 
 export default App;
