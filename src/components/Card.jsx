be72e15077c672f57087ab05dcb13cbcb268aeb5
Ralph Largo <devlargs@gmail.com>
2025-02-18 20:49:39 +0800

    feat: remove view details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,23 +14,6 @@ const Card = ({ name, imageSource }) => {
         </figure>
         <div className="article-body">
           <h2>{name}</h2>
-          {/* <p>{details || "N/A"}</p> */}
-          <p></p>
-          <a className="read-more">
-            View Details <span className="sr-only"></span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
         </div>
       </div>
     </article>
